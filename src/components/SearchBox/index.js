5285201acd4d19ee7f64d5963f0432c2d7a5044a
Irina Rogozhova <irina.rogozhova@saltpepper.ru>
2020-11-22 21:27:16 +0300

    Added search on enter press and autofocus to input

--- src/components/SearchBox/index.js
+++ src/components/SearchBox/index.js
@@ -9,14 +9,22 @@ const SearchBox = ({ onSearch }) => {
     onSearch(searchEntry);
   };
 
+  const handleKeypress = (e) => {
+    if (e.key === "Enter") {
+      handleSearch();
+    }
+  };
+
   return (
     <div className={styles.root}>
       <input
         type="search"
+        autoFocus
         className={styles.input}
         placeholder="Search for a city"
         value={searchEntry}
         onChange={(e) => setSearchEntry(e.target.value)}
+        onKeyPress={handleKeypress}
       />
       <span className={styles.searchBtn} onClick={handleSearch} />
     </div>
